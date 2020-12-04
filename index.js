81f085228aac778f21a2c49422fc9793c0c0fb4e
Benjamin Lavian <benylavian@gmail.com>
2020-12-04 10:34:49 -0500

    added even listener to teh button but running into an error where button is not defined

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const myWords = ["hello", "world", "ajax"];
 let player = {};
 
-const message = document.querySelector(".message");
-const output1 = document.querySelector("output1");
+const message = document.getElementsByClassName(".message");
+const output1 = document.getElementsByClassName(".output1");
 const output2= document.querySelector(".output2");
-const btn = document.querySelector("button");
+const btn = document.getElementById(button);
+
+
+btn.addEventListener("click", event , ()=> {
+myWords.sort(()=> {
+    return .5 - Math.random();
+});
+let theWord = myWords.shift()
+console.log(theWord)
+})
+
 
