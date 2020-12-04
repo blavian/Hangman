45d0d159f3a877c903fc096d0e6c1f99d547ecd4
Benjamin Lavian <benylavian@gmail.com>
2020-12-04 10:18:37 -0500

    query selectors for message, output1, output2 and button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const myWords = ["hello", "world", "ajax"];
+let player = {};
+
+const message = document.querySelector(".message");
+const output1 = document.querySelector("output1");
+const output2= document.querySelector(".output2");
+const btn = document.querySelector("button");
 
