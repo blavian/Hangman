d1fb4e0b4bff040b815c9efa9c91d137454c6455
Benjamin Lavian <benylavian@gmail.com>
2020-12-08 23:53:04 -0500

    fixed

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  const myWords = ["hello", "world", "ajax"];
+  const myWords = ['john', 'Javascript']
   let player = {};
   const message = document.querySelector(".message");
   const output1 = document.querySelector(".output1");
   const output2 = document.querySelector(".output2");
   const btn = document.querySelector("button");
-
   btn.addEventListener("click", function () {
     myWords.sort(function () {
       return 0.5 - Math.random();
     });
     let theWord = myWords.shift();
-    player.solution = theWord.split('');
+    player.solution = theWord.split("");
+    buildBoard();
     console.log(player.solution);
   });
 
-  const buildBoard =  ()=> {
-    playerSolution.forEach(letter => {
-        let div = document.createElement('div');
-        div.classList.add("letter2");
-        div.innerText = "_";
-        div.myLetter = letter;
-        output2.appendChild(div);
+  function buildBoard() {
+    player.solution.forEach(function (letter) {
+      let div = document.createElement("div");
+      div.classList.add("letter2");
+      div.innerText = "_";
+      div.myLetter = letter;
+      output2.appendChild(div);
     });
   }
+        
 });
 
 
